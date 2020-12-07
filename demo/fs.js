6734aa61ff7f92c0723494cae0b9241989e26c2b
Igor Seliversotv <igrseliwjorstow@bk.ru>
2020-12-07 22:11:35 +0400

    first commit

---
+++ demo/fs.js
@@ -0,0 +1,41 @@
+// fs - file system
+const fs = require('fs')
+const path = require('path')
+
+// fs.mkdir(path.join(__dirname, 'test'), (err) => {
+// 	if(err) {
+// 		throw err
+// 	}
+//
+// 	console.log('Папка создана')
+// })
+
+const filePath = path.join(__dirname, 'test', 'text.txt')
+
+// fs.writeFile(filePath, 'Hello, nodejs', err => {
+// 	if(err) {
+// 		throw(err)
+// 	}
+//
+// 	console.log('Файл создан')
+// })
+
+// fs.appendFile(filePath, '\nHello, nodejs', err => {
+// 	if(err) {
+// 		throw(err)
+// 	}
+//
+// 	console.log('Файл создан')
+// })
+
+fs.readFile(filePath, 'utf-8', (err, content) => {
+	if(err) {
+		throw err
+	}
+
+	// const data = Buffer.from(content)
+	//
+	// console.log(data.toString())
+
+	console.log(content)
+})
